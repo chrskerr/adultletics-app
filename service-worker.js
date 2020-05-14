989eac8f91a3114d52d19bb56033b2e138288873
Chris Kerr <chriskerr@me.com>
2020-05-15 08:41:51 +1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.28c50fa0effe7636a7228ebc679dfed8.js"
+  "/precache-manifest.58113d064bd835de5c3b6d5ba7ca09d4.js"
 );
 
 self.addEventListener('message', (event) => {
