c832e9e23a9076b2bd9b378cbeeed48a030a5542
Chris Kerr <chriskerr@me.com>
2020-08-31 11:31:51 +1000

    added text filter

--- src/views/views-admin/workouts/workouts.js
+++ src/views/views-admin/workouts/workouts.js
@@ -1,12 +1,12 @@
 
 // deps
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { Link } from "react-router-dom";
 import _ from "lodash";
 
 // app
-import { Badge, Pane, Text, Heading, Button } from "evergreen-ui";
+import { Badge, Pane, Text, Heading, Button, TextInputField } from "evergreen-ui";
 import { Services, Queries, Loading, constants } from "../index";
 
 //
@@ -16,22 +16,32 @@ import { Services, Queries, Loading, constants } from "../index";
 
 export default function Workouts () {
 	const { openPanel } = useContext( Services.UI );
+	const [ filter, setFilter ] = useState( "" );
 	const { data, loading } = useQuery( Queries.workouts.getAll );
 	const workouts = _.get( data, "workouts" );
 	const { intensityOptions, workoutTypes } = constants;
 
 	if ( loading ) return <Loading />;
 
+	const filteredWorkouts = filter ? _.filter( workouts, ({ title }) => _.lowerCase( title ).indexOf( _.lowerCase( filter )) !== -1 ) : workouts; 
+
 	return (
 		<>
 			<Pane display="flex" justifyContent="flex-end">
 				<Button iconBefore="plus" onClick={ () => openPanel({ panel: "workouts/add", size: "wide" })}>Add</Button>
 			</Pane>
-			<Pane marginBottom={ 16 }>
+			<Pane marginBottom={ 32 }>
 				<Heading>All Workouts:</Heading>
 			</Pane>
+			<Pane marginBottom={ 32 }>
+				<TextInputField 
+					label="Filter by workout title" 
+					placeholder="Start typing here..." 
+					onChange={ ({ target: { value }}) => setFilter( value ) }
+				/>
+			</Pane>
 			<Pane marginBottom={ 40 }>
-				{ workouts && _.map( workouts, workout => {
+				{ filteredWorkouts && _.map( filteredWorkouts, workout => {
 					const { id, title, intensity, type } = workout;
 					const intensityLabel = _.get( _.find( intensityOptions, [ "value", intensity ]), "label", "" );
 					const typeLabel = _.get( _.find( workoutTypes, [ "value", type ]), "label", "" );
